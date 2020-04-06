4071249512edb36b7a123dec68a38ab4c4d5be90
Jesus Seijas <jesus.seijas@axa.com>
2020-04-06 11:48:27 +0200

    build: remove ngrok, update deps

--- client/server/index.js
+++ client/server/index.js
@@ -6,7 +6,6 @@ const logger = require('./logger');
 const argv = require('minimist')(process.argv.slice(2));
 const setup = require('./middlewares/frontendMiddleware');
 const isDev = process.env.NODE_ENV !== 'production';
-const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
 const app = express();
 
@@ -31,17 +30,5 @@ app.listen(port, host, (err) => {
   if (err) {
     return logger.error(err.message);
   }
-
-  // Connect to ngrok in dev mode
-  if (ngrok) {
-    ngrok.connect(port, (innerErr, url) => {
-      if (innerErr) {
-        return logger.error(innerErr);
-      }
-
-      logger.appStarted(port, prettyHost, url);
-    });
-  } else {
-    logger.appStarted(port, prettyHost);
-  }
+  logger.appStarted(port, prettyHost);
 });
